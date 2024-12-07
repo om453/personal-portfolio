e63f5f5775eb6183fe2be13c2b33883bef98f2b4
Om Patel <om.patel200212@gmail.com>
2024-12-08 00:43:50 +0530

    added intro comp again for fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import WorkExperience from '@/components/WorkExperience';
 
 import Education from '@/components/Education';
 import Skills from '@/components/Skills';
+import Intro from '@/components/Intro';
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col md:gap-12 gap-8 md:p-24 p-10 relative overflow-hidden lg:w-7/12 sm:w-full mx-auto">
         <div className="fixed inset-0 pointer-events-none light-mode-fade-bottom dark:fade-bottom z-10 bg-white/10 dark:bg-black/10" />
-
+        <Intro /> 
         <WorkExperience />
         <Education />
         <Skills />
