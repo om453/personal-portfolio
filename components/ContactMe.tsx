17d757b364e7a80329af7832c690ae0cfcbf8f66
Om Patel <om.patel200212@gmail.com>
2024-12-11 14:08:15 +0530

    removed unsual comments

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -3,27 +3,6 @@ import React from "react";
 
 function ContactMe() {
   return (
-    // <div className="flex flex-col gap-4 mb-20">
-    //   <h1 className="text-2xl font-bold">Contact Me</h1>
-    //   <p className="text-sm text-gray-500 dark:text-gray-300">
-    //     Want to chat? Just shoot me a dm with a{" "}
-    //     <Link
-    //       href="https://x.com/Om1111X"
-    //       target="_blank"
-    //       className="text-blue-500"
-    //     >
-    //       direct question on twitter
-    //     </Link>{" "}
-    //     or email me directly at{" "}
-    //     <Link
-    //       href="mailto:om.patel200212@gmail.com"
-    //       className="text-blue-500"
-    //     >
-    //       om.patel200212@gmail.com
-    //     </Link>
-    //   </p>
-    // </div>
-
     <section id="contact">
     <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
         <div className="space-y-3">
